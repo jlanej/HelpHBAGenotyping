1adcaf1eeea97cf23db32f71cbfb7cbb4a159747
Mark Chaisson <mchaisso@usc.edu>
2025-11-18 15:25:03 -0800

    Updated shell script to force diploid calling

--- ResultToVcf.sh
+++ ResultToVcf.sh
@@ -7,6 +7,10 @@ sa=`echo $aRes | tr "_" "\t" | awk '{ print $2;}'`
 sb=`echo $bRes | tr "_" "\t" | awk '{ print $2;}'`
 ha=`echo $aRes | tr "_" "\t" | awk '{ print $3;}' | tr -d "h"`
 hb=`echo $bRes | tr "_" "\t" | awk '{ print $3;}' | tr -d "h"`
+if [[ $sb -eq "" ]]; then
+	sb=$sa
+	hb=$ha
+fi
 CombineVcfs.py  --hap1 $vcfPath/$sa.hap"$ha".var.vcf --hap2 $vcfPath/$sb.hap"$hb".var.vcf
 
 
